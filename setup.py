c40b3793e8a67eec4e14071aad1cfa77efa05481
shivam_uppal <shivsen1997@gmail.com>
2024-06-18 22:08:29 +0530

    requirements.txt and setup.py added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages,  setup
+
+setup(name="Medical_chatbot",
+      version='0.0.1',
+      author="shiv",
+      packages=find_packages(),
+      install_requires=[])
